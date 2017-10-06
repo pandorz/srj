d3e65b15c7ff896ab512ce09935c99d04f80a426
Lucie Guilbert <lucie@lesterius.com>
2017-10-06 10:30:12 +0200

    add: blog to timeline dashboard BO

--- src/AppBundle/Repository/BlogRepository.php
+++ src/AppBundle/Repository/BlogRepository.php
@@ -14,4 +14,31 @@ namespace AppBundle\Repository;
  */
 class BlogRepository extends \Doctrine\ORM\EntityRepository
 {
+    public function findAllValidOverOneMonth($admin = false)
+    {
+        if ($admin) {
+            $query = $this
+                ->getEntityManager()
+                ->createQuery('SELECT e '
+                    . 'FROM AppBundle:Blog e '
+                    . 'WHERE e.timestampCreation >= :dateFin '
+                    . 'ORDER BY e.timestampCreation DESC')
+                ->setParameter('dateFin', date("Y-m-d",strtotime("-1 month")));
+        } else {
+            $query = $this
+                ->getEntityManager()
+                ->createQuery('SELECT e '
+                    . 'FROM AppBundle:Blog e '
+                    . 'WHERE e.affiche = :affiche '
+                    . 'AND e.datePublication <= :datePublication '
+                    . 'AND e.timestampCreation >= :dateFin '
+                    . 'ORDER BY e.timestampCreation DESC')
+                ->setParameters([
+                    'affiche' => true,
+                    'datePublication' => date("Y-m-d"),
+                    'dateFin' => date("Y-m-d",strtotime("-1 month"))
+                ]);
+        }
+        return $query->getResult();
+    }
 }
