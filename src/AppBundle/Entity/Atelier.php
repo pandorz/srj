604f7bc8045b97dd294b8b828831ccbe41c3a2b9
Lucie Guilbert <lucie@lesterius.com>
2017-10-06 15:07:30 +0200

    fix: oubli asserts

--- src/AppBundle/Entity/Atelier.php
+++ src/AppBundle/Entity/Atelier.php
@@ -6,6 +6,8 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Sonata\MediaBundle\Model\MediaInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Oh\GoogleMapFormTypeBundle\Validator\Constraints as OhAssert;
 
 /**
  * Atelier
