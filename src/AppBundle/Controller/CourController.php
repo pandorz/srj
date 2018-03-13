d13ec4c08eda01103579b160bb70352519328903
Pandorz <pandorz.yahourt@gmail.com>
2018-03-13 17:15:31 +0100

    add: button generate calendrier sur cours

--- src/AppBundle/Controller/CourController.php
+++ src/AppBundle/Controller/CourController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * Class CourController
@@ -9,5 +11,23 @@ namespace AppBundle\Controller;
  */
 class CourController extends BaseController
 {
+    public function generateGoogleCalendarAction($id)
+    {
+        $cour = $this->admin->getSubject();
 
+        if (!$cour) {
+            throw new NotFoundHttpException(sprintf('unable to find the object with id: %s', $id));
+        }
+
+        $googleCalendar = $this->container->get('app.generate_calendar');
+        $googleCalendar->newCalendar($cour);
+
+
+        $this->addFlash(
+            'sonata_flash_success',
+            'Un nouveau calendrier a été créé dans le compte Google de l\'association'
+        );
+
+        return new RedirectResponse($this->admin->generateUrl('list'));
+    }
 }
