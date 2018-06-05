cbcb503ac49bcd62ee6dc5ab74b42b858ef7d3fb
pandorz <pandorz.yahourt@gmail.com>
2018-06-05 09:50:30 +0200

    mod: login impossible de cliquer deux fois + modification du curseur

--- app/Resources/assets/js/login.js
+++ app/Resources/assets/js/login.js
@@ -9,23 +9,29 @@ app.loginController.defaultAction = {
 
     initlogin: function() {
         $('body').on( 'click', '#_submit', function(e){
-            var $form = $('.js-form-login');
+            var $form   = $('.js-form-login');
+            var $button = $(this);
             e.preventDefault();
-            $.ajax({
-                type        : $form.attr( 'method' ),
-                url         : $form.attr( 'action' ),
-                data        : $form.serialize(),
-                dataType    : "json",
-                error: function(xhr, status, error){
-                    var err = eval("(" + xhr.responseText + ")");
-                    var $errorMessage = $('.js-form_login_error');
-                    $errorMessage.find('.alert').html(err.message);
-                    $errorMessage.show();
-                },
-                success: function (data) {
-                    window.location.href = data.path;
-                }
-            });
+
+            if (!$button.hasClass('disabled')) {
+                $button.addClass('wait-cursor disabled');
+                $.ajax({
+                    type: $form.attr('method'),
+                    url: $form.attr('action'),
+                    data: $form.serialize(),
+                    dataType: "json",
+                    error: function (xhr, status, error) {
+                        var err = eval("(" + xhr.responseText + ")");
+                        var $errorMessage = $('.js-form_login_error');
+                        $errorMessage.find('.alert').html(err.message);
+                        $errorMessage.show();
+                        $button.removeClass('wait-cursor disabled');
+                    },
+                    success: function (data) {
+                        window.location.href = data.path;
+                    }
+                });
+            }
         });
 
     }
