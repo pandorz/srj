9bffc249c6c723e15f389ddcb5d8e1293e325024
pandorz <pandorz.yahourt@gmailcom>
2018-06-06 16:19:16 +0200

    mod: formulaire mdp oublié & demande de compte html/css/js

--- app/Resources/assets/js/login.js
+++ app/Resources/assets/js/login.js
@@ -8,13 +8,20 @@ app.loginController.defaultAction = {
      */
 
     initlogin: function() {
-        $('body').on( 'click', '#_submit', function(e){
+        var $errorMessage       = $('.js-form_login_error');
+        var $errorMessageAlert  = $errorMessage.find('.alert');
+        $errorMessageAlert.html('');
+        $errorMessage.hide();
+
+        $('body').on( 'click', '#_submit_login', function(e){
             var $form   = $('.js-form-login');
             var $button = $(this);
             e.preventDefault();
 
             if (!$button.hasClass('disabled')) {
                 $button.addClass('wait-cursor disabled');
+                $errorMessageAlert.html('');
+                $errorMessage.hide();
                 $.ajax({
                     type: $form.attr('method'),
                     url: $form.attr('action'),
@@ -22,8 +29,7 @@ app.loginController.defaultAction = {
                     dataType: "json",
                     error: function (xhr, status, error) {
                         var err = eval("(" + xhr.responseText + ")");
-                        var $errorMessage = $('.js-form_login_error');
-                        $errorMessage.find('.alert').html(err.message);
+                        $errorMessageAlert.html(err.message);
                         $errorMessage.show();
                         $button.removeClass('wait-cursor disabled');
                     },
@@ -33,13 +39,123 @@ app.loginController.defaultAction = {
                 });
             }
         });
+    },
 
+    initPwd: function () {
+        var $containerMessage       = $('.js-form_lost_pwd_messages');
+        var $errorMessageAlert      = $containerMessage.find('.alert');
+        var $successMessageAlert    = $containerMessage.find('.success');
+        this.resetMessage($successMessageAlert, $errorMessageAlert);
+        this.formActionAccessPwd($successMessageAlert, $errorMessageAlert, '_submit_pwd', 'js-form-lost-pwd');
+    },
+
+    initAccess: function () {
+        var $containerMessage       = $('.js-form_ask-account_messages');
+        var $errorMessageAlert      = $containerMessage.find('.alert');
+        var $successMessageAlert    = $containerMessage.find('.success');
+        this.resetMessage($successMessageAlert, $errorMessageAlert);
+        this.formActionAccessPwd($successMessageAlert, $errorMessageAlert, '_submit_access', 'js-form-ask-account');
+    },
+
+    resetMessage: function ($successMessageAlert, $errorMessageAlert) {
+        $errorMessageAlert.html('');
+        $successMessageAlert.html('');
+        $errorMessageAlert.hide();
+        $successMessageAlert.hide();
+    },
+
+    formActionAccessPwd: function ($successMessageAlert, $errorMessageAlert, idButton, classForm) {
+        var self = this;
+        $('body').on( 'click', '#'+idButton, function(e){
+            var $form   = $('.'+classForm);
+            var $button = $(this);
+            e.preventDefault();
+
+            if (!$button.hasClass('disabled')) {
+                $button.addClass('wait-cursor disabled');
+                self.resetMessage($successMessageAlert, $errorMessageAlert);
+                $.ajax({
+                    type: $form.attr('method'),
+                    url: $form.attr('action'),
+                    data: $form.serialize(),
+                    dataType: "json",
+                    error: function (xhr, status, error) {
+                        var err = eval("(" + xhr.responseText + ")");
+                        $errorMessageAlert.html(err.message);
+                        $errorMessageAlert.show();
+                        $button.removeClass('wait-cursor disabled');
+                    },
+                    success: function (data) {
+                        $successMessageAlert.html(data.message);
+                        $successMessageAlert.show();
+                        $button.removeClass('wait-cursor disabled');
+                    }
+                });
+            }
+        });
+    },
+
+    initNavButtons: function () {
+        var $body = $('body');
+        var self = this;
+
+        $body.on( 'click', '.js-show-login-form', function(e) {
+            self.showLoginForm();
+            self.hideAccesForm();
+            self.hideLostPwdForm();
+        });
+
+        $body.on( 'click', '.js-show-pwd-form', function(e) {
+            self.showLostPwdForm();
+            self.hideLoginForm();
+            self.hideAccesForm();
+
+        });
+
+        $body.on( 'click', '.js-show-access-form', function(e) {
+            self.showAccesForm();
+            self.hideLoginForm();
+            self.hideLostPwdForm();
+        });
+    },
+
+    showLoginForm: function () {
+        $('.js-form-login').removeClass('u-hidden');
+        $('.js-show-login-form').addClass('u-hidden');
+        this.initlogin();
+    },
+
+    showLostPwdForm: function () {
+        $('.js-form-lost-pwd').removeClass('u-hidden');
+        $('.js-show-pwd-form').addClass('u-hidden');
+        this.initPwd()
+    },
+
+    showAccesForm: function () {
+        $('.js-form-ask-account').removeClass('u-hidden');
+        $('.js-show-access-form').addClass('u-hidden');
+        this.initAccess();
+    },
+
+    hideLoginForm: function() {
+        $('.js-form-login').addClass('u-hidden');
+        $('.js-show-login-form').removeClass('u-hidden');
+    },
+
+    hideLostPwdForm: function () {
+        $('.js-form-lost-pwd').addClass('u-hidden');
+        $('.js-show-pwd-form').removeClass('u-hidden');
+    },
+
+    hideAccesForm: function () {
+        $('.js-form-ask-account').addClass('u-hidden');
+        $('.js-show-access-form').removeClass('u-hidden');
     }
 };
 
 $(document).ready(function(){
     if ('.js-form-login') {
         app.loginController.defaultAction.initlogin();
-        $('.js-form_login_error').hide();
+        app.loginController.defaultAction.initNavButtons();
     }
 });
