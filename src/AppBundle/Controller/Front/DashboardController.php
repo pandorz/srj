69c8d9650120398d1c840c75cb76d35027d7a696
pandorz <pandorz.yahourt@gmailcom>
2018-06-10 23:10:53 +0200

    add: ckeditor skin add: add/edit article FO todo: style + tag

--- src/AppBundle/Controller/Front/DashboardController.php
+++ src/AppBundle/Controller/Front/DashboardController.php
@@ -7,6 +7,7 @@ use AppBundle\Entity\Cour;
 use AppBundle\Entity\DemandeNewsletter;
 use AppBundle\Entity\Partenaire;
 use AppBundle\Entity\Utilisateur;
+use AppBundle\Form\BlogType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -40,5 +41,41 @@ class DashboardController extends BaseController
             return $this->redirectToRoute('fos_user_security_logout');
         }
         return $this->render('front/users/my_space/dashboard.html.twig');
+    }
+
+    /**
+     * Dashboard
+     *
+     * -------------------- *
+     * @Route("/article", name="add_article")
+     * @Route("/article/{slug}", name="edit_article")
+     * @Method({"GET", "POST"})
+     * -------------------- *
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function blogAction(Request $request, $slug = null)
+    {
+        if (!is_null($slug)) {
+            $blog = $this->getEm()->getRepository(Blog::class)->findOneBySlug($slug);
+        }
+
+        if (is_null($slug) || (isset($blog) && empty($blog))) {
+            $blog = new Blog();
+            $blog->addAuteur($this->getUser());
+        }
+
+        $form = $this->createForm(BlogType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $form->bindRequest($request);
+            // TODO tag
+            // TODO workflow
+            $this->getEm()->persist($blog);
+            $this->getEm()->flush();
+        }
+
+        return $this->render('front/users/my_space/article_form.html.twig', ['form' => $form->createView()]);
     }
 }
